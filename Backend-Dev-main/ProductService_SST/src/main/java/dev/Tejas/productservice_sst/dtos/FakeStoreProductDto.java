d1bd95c0ea3ce7259f9fd77753be16733c69d589
Tejas Ghatule <141946130+CodeMaverick2@users.noreply.github.com>
2024-04-08 21:51:01 +0530

    Add files via upload

---
+++ Backend-Dev-main/ProductService_SST/src/main/java/dev/Tejas/productservice_sst/dtos/FakeStoreProductDto.java
@@ -0,0 +1,18 @@
+package dev.Tejas.productservice_sst.dtos;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class FakeStoreProductDto {
+    private Long id;
+    private String title;
+    private String description;
+    private double price;
+    private String category;
+    private String image;
+
+}
